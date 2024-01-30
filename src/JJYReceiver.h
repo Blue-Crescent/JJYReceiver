e54a7b8f1117f33ae55857451f9be4c072b506d8
BlueCrescent <ick40195@gmail.com>
2024-01-30 22:52:44 +0900

    payloadlength check

--- src/JJYReceiver.h
+++ src/JJYReceiver.h
@@ -12,42 +12,19 @@
 #ifdef DEBUG_BUILD
 #include <SoftwareSerial.h>
 extern SoftwareSerial debugSerial;
-# define DEBUG_PRINT(fmt,...)  debugSerial.print(fmt);
-# define DEBUG_PRINTLN(fmt,...) debugSerial.println(fmt);
+# define DEBUG_PRINT(...)  debugSerial.print(__VA_ARGS__);
+# define DEBUG_PRINTLN(...) debugSerial.println(__VA_ARGS__);
 #else
 # define DEBUG_PRINT(fmt,...)
 # define DEBUG_PRINTLN(fmt,...)
 #endif
+
 
 enum STATE {INIT,BITSYNC,RECEIVE,DECODE,TIME,TIMESYNCED};
 enum JJYSTATE {JJY_INIT=-1,JJY_MIN=0,JJY_HOUR=1,JJY_DOYH=2,JJY_DOYL=3,JJY_YEAR=4,JJY_WEEK=5};
 typedef union {
     uint8_t datetime[8];
     struct {
-//         uint8_t M : 1 ;
-//         uint8_t min : 8 ;
-//         uint8_t P1 : 1 ;
-//         uint16_t hour : 9 ;
-//         uint8_t P2 : 1 ;
-//         uint8_t space1 : 2 ;
-//         uint16_t doyh : 9 ;
-//         uint8_t P3 : 1 ;
-//         uint8_t doyl : 4 ;
-//         uint8_t space2 : 2 ;
-//         uint8_t parity : 2 ;
-//         uint8_t su1 : 1 ;
-//         uint8_t P4 : 1;
-//         uint8_t su2 : 1 ;
-//         uint8_t year : 8;
-//         uint8_t P5 : 1;
-//         uint8_t weekday : 3;
-//         uint8_t leap : 2;
-//         uint8_t space3 : 4;
-//         uint8_t P0 : 1;
-
-//         uint8_t dummy : 4;
-//     } bits;
-// } JJYData;
         uint8_t dummy : 4;
         uint8_t P0 : 1;
         uint8_t space3 : 4;
@@ -85,23 +62,17 @@ class JJYReceiver {
     volatile enum JJYSTATE jjystate = JJY_INIT;
     volatile uint8_t jjyoffset = 0;
     volatile uint8_t jjypos = 0;
-    volatile uint16_t jjybits[6]; // 8bits bit data between marker
+    volatile uint16_t jjypayload[6]; // 8bits bit data between marker
+    volatile uint8_t jjypayloadlen[6]; // 
 
     volatile uint8_t sampleindex = 0;
     volatile uint8_t sampling [13] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-    // volatile uint8_t MASK [13]     = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
     volatile uint8_t CONST_PM [13] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xF0,0x00,0x00};
     volatile uint8_t CONST_H [13]  = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xF8,0x00,0x00,0x00,0x00,0x00,0x00,};
-    //                                  F8   00   00   00   00   00   01   FF   FF   FF   FF   FF   FF
     volatile uint8_t CONST_L [13]  = {0x0F,0xFF,0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
    
     char rcvcnt = 0;
-    // int pulsewidth;
     volatile uint8_t bitcount = 0;
-    
-    // volatile unsigned long diff[2];
-    // volatile unsigned long edge[3];
-    // uint8_t edgecnt=0;
     volatile unsigned long syncmillis;
 
     int monitorpin = -1;
@@ -134,7 +105,6 @@ class JJYReceiver {
     int rotateArray(int8_t diff, uint8_t* array, uint8_t size);
     int calculateDate(uint16_t year, uint8_t dayOfYear, uint8_t *month, uint8_t *day);
     int receive();
-    //int distance(uint16_t x, uint16_t y);
     int distance(uint8_t* arr1, uint8_t* arr2, int size);
     int max_of_three(uint8_t a, uint8_t b, uint8_t c);
     bool calculateParity(uint8_t value, uint8_t bitLength, uint8_t expectedParity);
@@ -143,11 +113,6 @@ class JJYReceiver {
     int printJJYData(const JJYData& data);
     int debug();
     #endif
-
-// time code position marker mask
-//  100000000010000000001000000000100000000010000000001000000001 
-//  1000_0000_0010_0000_0000_1000_0000_0010_0000_0000_1000_0000_0010_0000_0001 
-//-> 0x802008020080201
 
   private:
     //unsigned long margin = 60;
@@ -162,11 +127,6 @@ class JJYReceiver {
       
       uint16_t yday = ((((jjydata[index].bits.doyh >> 5) & 0x0002)) * 100) + (((jjydata[index].bits.doyh & 0x000f)) * 10) + jjydata[index].bits.doyl;
       calculateDate(year, yday ,&timeinfo.tm_mon, &timeinfo.tm_mday);
-      DEBUG_PRINT(yday);
-      DEBUG_PRINT("-");
-      DEBUG_PRINT(timeinfo.tm_mon+1);
-      DEBUG_PRINT("/");
-      DEBUG_PRINTLN(timeinfo.tm_mday);
       timeinfo.tm_hour  = ((jjydata[index].bits.hour >> 5) & 0x3) * 10 + (jjydata[index].bits.hour & 0x0f) ;         // 時
       timeinfo.tm_min   = ((jjydata[index].bits.min >> 5) & 0x7)  * 10 + (jjydata[index].bits.min & 0x0f) + 1;          // 分
       timeinfo.tm_sec   = 1;           // 秒
