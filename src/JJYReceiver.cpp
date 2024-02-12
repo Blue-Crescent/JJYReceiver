6fd867ecf449e5e5f948afa674e1978914fb497f
BlueCrescent <ick40195@gmail.com>
2024-02-12 11:54:43 +0900

    画像追加

--- src/JJYReceiver.cpp
+++ src/JJYReceiver.cpp
@@ -34,8 +34,10 @@ JJYReceiver::~JJYReceiver(){
 }
 
 time_t JJYReceiver::clock_tick(){
-  timeinfo.tm_sec = (timeinfo.tm_sec + 1) % 60;           // 秒
   globaltime = globaltime + 1;
+  if(state != RECEIVE)  return globaltime;
+  timeinfo.tm_sec = (timeinfo.tm_sec + 1) % 60;           // 秒
+  if(timeinfo.tm_sec = 0) timeinfo.tm_mon = (timeinfo.tm_mon + 1) % 60; // 分
   return globaltime;
 }
 
